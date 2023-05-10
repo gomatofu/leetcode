bf8b0d1a3fdcd66a79e670a85cb53351f4c97eb4
goto <gogo8bit@yahoo.co.jp>
2023-05-10 11:08:10 +0900

    20. Valid Parentheses

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dict = {')': '(', ']':'[', '}': '{'}
+        stack = []
+
+        for c in s:
+            if c in dict.values():
+                stack.append(c)
+            elif stack != [] and dict[c] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+
+        return stack == []
