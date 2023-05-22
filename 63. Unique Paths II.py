1549dbcb7c224a1a838f42ac7c8e00899b307032
goto <gogo8bit@yahoo.co.jp>
2023-05-22 17:28:26 +0900

    63. Unique Paths II

--- 63. Unique Paths II.py
+++ 63. Unique Paths II.py
@@ -2,17 +2,22 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:              
         m, n = len(obstacleGrid), len(obstacleGrid[0])        
         
-        dp = [[0] * n for _ in range(m)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
         
-        if obstacleGrid[0][0] == 0:
-            dp[0][0] = 1
+        for c in range(n):
+            if obstacleGrid[0][c] == 1: 
+                break
+            dp[0][c] = 1
+
+        for r in range(m):
+            if obstacleGrid[r][0] == 1: 
+                break
+            dp[r][0] = 1
                         
-        for row in range(m):
-            for col in range(n):
-                if obstacleGrid[row][col] == 0:
-                    if row > 0:
-                        dp[row][col] += dp[row-1][col]
-                    if col > 0:
-                        dp[row][col] += dp[row][col-1]
-         
+        for r in range(1,m):
+            for c in range(1,n):
+                if obstacleGrid[r][c] == 1:
+                    continue
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+
         return dp[-1][-1]
